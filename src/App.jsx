9b6a4a1aa0181619d39e1a0f4f1c82a08bad3479
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2023-12-07 13:38:11 +0530

    bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable no-unused-vars */
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
-import TemperatureAndDetails from "./components/TemperatureAndDetails";
-import Forecast from "./components/Forecast";
-import getWeatherData from "./utils/constants";
+
 import { useEffect, useState } from "react";
-import Cards from "./components/Cards";
 import CardContainer from "./components/CardContainer";
+import EmptyPage from "./components/EmptyPage";
+import Error from "./components/Error";
 
 
 
@@ -15,19 +14,28 @@ const App = () => {
   const [weather, setWeather] = useState();
   const [forecast, setForecast] = useState();
 
- 
-
 
   return (
     <div className="mx-auto max-w-full py-5 px-6 lg:px-32
      bg-gradient-to-br from-cyan-700 to-blue-400 shadow-xl
      shadow-gray-700 min-h-screen flex flex-col">
-  <Inputs weather={weather} setWeather={setWeather} setForecast={setForecast} />
-  <TimeAndLocation weather={weather} />
-  <CardContainer forecast={forecast} />
+  {weather ? (
+  <>
+   <Inputs weather={weather} setWeather={setWeather} setForecast={setForecast} />
+    <TimeAndLocation weather={weather} />
+    <CardContainer forecast={forecast} />
+  </>
+) : (
+  <>
+    <Inputs weather={weather} setWeather={setWeather} setForecast={setForecast} />
+    <EmptyPage />
+    </>
+
+)}
+
+
 </div>
   );
- 
   };
 
 
---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+
+
+const Error = () => {
+  return (
+    <div>
+        <h1 className="font-bold text-9xl">
+            Search Weather for any City
+        </h1>
+    </div>
+  )
+}
+
+export default Error
